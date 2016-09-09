532a3e6dc10bd50efa294ed92430918fcb160ea6
Bruno Ferrari <b_ferrari@live.com>
2016-09-09 20:52:36 -0300

    fixed a bunch of bugs and some refactoring

--- widget.js
+++ widget.js
@@ -2,10 +2,18 @@ var ba = chrome.browserAction;
 var intervalTime = Math.random() * 3000 * 60;
 
 var baseUrl = 'https://review-api.udacity.com';
-var token = 'api-token-here';
+var token = 'api-key-here';
 
 var data;
-var project = {};
+var notificated = [];
+
+function Project(id, name, rate, quantity, lang) {
+		this.id = id;
+		this.name = name;
+		this.rate = rate;
+		this.quantity = quantity;
+		this.lang = lang;
+	}
 
 chrome.browserAction.onClicked.addListener(function(tab) {
 	var destination = 'https://review.udacity.com/#!/submissions/dashboard';
@@ -13,9 +21,9 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 });
 
 function getRecentProjects() {
-	var avaiableProjectsPtBr = 0;
-
+	var availableReviews = 0;
 	var request = new XMLHttpRequest();
+
 	request.open('GET', baseUrl + '/api/v1/me/certifications', true);
 	request.setRequestHeader('Authorization', token)
 
@@ -23,18 +31,14 @@ function getRecentProjects() {
 		data = JSON.parse(request.responseText);
 		if (request.status >= 200 && request.status < 400) {
 			for (i = 0; i < data.length; i++) {
+				var project = data[i].project;
 				if (isCertified() && isActiveProject()) {
-					logListenProjects(data);
-					if (data[i].project['awaiting_review_count_by_language']['pt-br'] > 0) {
-						avaiableProjectsPtBr ++;
-						project['name'] = data[i].project['name']
-						project['rate'] = 0.0
-						createNotification(project);
-					}
+					availableReviews += getAwaitingReviewCountByLanguage(project, 'pt-br');
+					logListeningProjects(data);
+					notify(project, 'pt-br');
 				}
 			}
-			updateNotificationStatus(avaiableProjectsPtBr);
-			// createNotification(data.project['name']);
+			updateNotificationStatus(availableReviews);
 		} else {
 			// TODO: handle error
 			console.log(data);
@@ -50,16 +54,47 @@ function getRecentProjects() {
 	setTimeout(getRecentProjects, intervalTime);
 }
 
+function getAwaitingReviewCountByLanguage(project, lang) {
+	var count = project['awaiting_review_count_by_language'][lang];
+	return count > 0 ? count : 0;
+}
+
+function notify(project, lang) {
+	if (project['awaiting_review_count_by_language'][lang] > 0) {
+		var id = project['project_id'];
+		var name = project['name'];
+		var rate = project['price'];
+		var qty = project['awaiting_review_count_by_language'][lang];
+		var lang = lang;
+
+		var proj = new Project(id, name, rate, qty, lang);
+
+		createNotification(proj);
+	}			
+}
+
+function isAlreadyNotificated(project) {
+	for (i = 0; i < notificated.lenght; i++) {
+		var obj = notificated[i];
+
+		if (project.id == obj.id) {
+			return true;
+		}
+		return false;
+	}
+}
+
 function createNotification(project) {
 	var opt = {
 	  type: "basic",
-	  title: "Review Available",
-	  message: project.name + " Rate: " + project.rate,
+	  title: "Awaiting Review",
+	  message: project.name + " Rate: $" + parseInt(project.rate, 10) 
+	  		+ '\nQuantity: ' + project.quantity + ' Language: ' + project.lang,
 	  iconUrl: "icon.png"
 	}
 
 	chrome.notifications.create("example", opt, function callback(){
-		console.log("asdasda")
+		console.log("Notification created!");
 	});
 }
 
@@ -73,15 +108,17 @@ function addCounterBadge(itemCount) {
 	ba.setBadgeText({text: '' + itemCount});
 }
 
-function logListenProjects(data) {
+function logListeningProjects(data) {
+	var formattedDateTime = formatDateTime(new Date());
+
 	var ptReviews = data[i].project['awaiting_review_count_by_language']['pt-br'];
 	var enReviews = data[i].project['awaiting_review_count_by_language']['en-us'];
-	var chReviews = data[i].project['awaiting_review_count_by_language']['ch-zn'];
-	var msg = "listening project " + data[i].project['name'] + " with " 
+	var chReviews = data[i].project['awaiting_review_count_by_language']['zh-cn'];
+	var msg = formattedDateTime + " listening project " + data[i].project['name'] + " with " 
 				+ data[i].project['awaiting_review_count'] + " reviews pending globally \n" 
 				+ checkReviewsByLanguage(ptReviews) + " review(s) in pt-br\n"
 				+ checkReviewsByLanguage(enReviews) + " review(s) in en-us\n"
-				+ checkReviewsByLanguage(chReviews) + " review(s) in ch-zn\n";
+				+ checkReviewsByLanguage(chReviews) + " review(s) in zh-cn\n";
 	console.log(msg);
 }
 
@@ -118,6 +155,17 @@ function hasAwaitingReviewsInPtBr() {
 
 function haveNotAvailableReviews(){
 	return data[i]['awaiting_review_count'] == 0;
+}
+
+function formatDateTime(date) {
+	var hours = date.getHours();
+	var formattedHours = hours % 12 || 12;
+	var minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+	var seconds = date.getSeconds();
+	var ampm = hours >= 12 ? 'PM' : 'AM';
+	var now = formattedHours + ':' + minutes + ':' + seconds +  ' ' + ampm;
+
+	return date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear() + ' at ' + now;
 }
 
 getRecentProjects();
